124b1a6e61b240802dddfcdfde013a4397ef0ea8
JB <rubiojeanbaptiste@gmail.com>
2021-03-10 17:27:50 +0100

    add DataGenerator

--- lib/formatter/DataGenerator.py
+++ lib/formatter/DataGenerator.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+import random
+import xlsxwriter
+from datetime import datetime
 
 
 def readCampus(path):
@@ -7,6 +10,7 @@ def readCampus(path):
         campus = pd.read_excel(
             path, sheet_name='Nombre campus', header=3, index_col=0, engine='openpyxl')
         campus = campus.loc[:, ['Liste']]
+        print('campus ok')
         return campus
 
     except OSError as e:
@@ -17,6 +21,7 @@ def readCampus(path):
 def readForecast(path):
     try:
         data = pd.read_excel(path, sheet_name='Real Model', engine='openpyxl')
+        print('forecast ok')
         return data
 
     except OSError as e:
@@ -86,15 +91,94 @@ def campusCompletion(path, campusDataFrame):
     for i in range(0, len(campusDataFrame)):
         n = campusDataFrame.iloc[i, 2]//PRODUCER_SCOPE
         supplierList.append(n)
-        campusDataFrame['N. Producteurs'] = supplierList
+    campusDataFrame['N. Producteurs'] = supplierList
 
     return campusDataFrame
 
 
+def createProducerDataFrame(campusDataFrame):
+    tabCamp = []
+    tabProd = []
+
+    for name in campusDataFrame['Liste']:
+        val = campusDataFrame.loc[campusDataFrame['Liste']
+                                  == name, 'N. Producteurs'].iloc[0]
+        for n in range(0, val):
+            char = "Prod_" + str(n)
+            tabCamp.append(name)
+            tabProd.append(char)
+
+    producers = pd.DataFrame(tabCamp, columns=['Id_Campus'])
+    producers['Id_Prod'] = pd.DataFrame(tabProd)
+
+    return producers
+
+
+def uniformDistribution(forecastDataFrame, producerDataFrame):
+    iterator = forecastDataFrame.columns.values.tolist()
+    iterator.remove('date')
+    orderList = {}
+
+    for camp in iterator:
+        # Caclul des paramètres de chaque campus
+        n = producerDataFrame['Id_Campus'].str.count(camp).sum()
+        p = 1/n
+        buffer = pd.DataFrame({'date': forecastDataFrame['date']})
+        prod = producerDataFrame.loc[producerDataFrame['Id_Campus']
+                                     == camp, 'Id_Prod']
+        orders = []
+
+        # Calcul du nombre de commandes pour chaque jour de l'année
+        # et pour chaque producteur suivant une loi uniforme
+        for i in range(len(forecastDataFrame[camp])):
+            consumers = round(forecastDataFrame[camp][i]*p)
+            orders.append(consumers)
+
+        # Pour chaque jour et chaque campus, ajout de la quantité de commande pour
+        for element in prod:
+            buffer[element] = orders
+
+        orderList[camp] = buffer
+
+    return orderList
+
+
+def addWeight(dicOrders):
+    iterator = []
+    meal = [1, 2, 3, 4, 5, 6]
+
+    for camp in dicOrders:
+        # campName est le campus courant sous forme de string
+        #     print(camp)
+
+        iterator = dicOrders[camp].columns.values.tolist()
+        iterator.remove('date')
+        # iterator est la liste des producteurs pour chaque campus
+
+        for prod in iterator:
+
+            for day in dicOrders[camp]['date']:
+                amount = 0
+                if not (dicOrders[camp].loc[dicOrders[camp]['date'] == day, prod].iloc[0] == 0):
+                    for consummer in range(0, dicOrders[camp].loc[dicOrders[camp]['date'] == day, prod].iloc[0]):
+                        amount += random.choice(meal)
+                    print(camp, amount)
+                    dicOrders[camp].loc[dicOrders[camp]
+                                        ['date'] == day, prod] = amount
+                    amount = 0
+
+    return dicOrders
+
+
+def writeFile(writingPath, dicWeights):
+    with pd.ExcelWriter(writingPath, engine='xlsxwriter', datetime_format='dd mm yyyy') as writer:
+        for camp in dicWeights:
+            dicWeights[camp].to_excel(writer, sheet_name=camp)
+
+
 if __name__ == "__main__":
     # File path delaration
-    path = r'res\\formatter\\example.xlsm'
-    # path = 'example.xlsm'
+    path = r'res\formatter\forecast.xlsm'
 
     campusDataFrame = readCampus(path)
     forecastDataFrame = readForecast(path)
@@ -103,5 +187,12 @@ if __name__ == "__main__":
     dfOrders = preprocessing(dfOrders, forecastDataFrame, campusDataFrame)
 
     dfCampus = campusCompletion(path, campusDataFrame)
+    dfProducer = createProducerDataFrame(dfCampus)
 
-    print(dfCampus.head(50))
+    dicOrders = uniformDistribution(dfOrders, dfProducer)
+    dicWeights = addWeight(dicOrders)
+
+    writingPath = r'res\routing\DailyGroupedOrders.xlsx'
+    writeFile(writingPath, dicWeights)
+
+    print(dicWeights)
