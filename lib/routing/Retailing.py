6f260b4bca84eb68bda93877b115dbe1443dcfdd
JB <rubiojeanbaptiste@gmail.com>
2021-03-15 01:49:03 +0100

    add Franck's requirements

--- lib/routing/Retailing.py
+++ lib/routing/Retailing.py
@@ -82,7 +82,6 @@ def getCosts(dicToProcess, date=['04/01/22'], campus=['G. Charpak']):
                 res[day] = bufferDate
         return res
     else:
-
         for camp in campus:
             bufferCampus = {}
             for day in date:
@@ -102,7 +101,66 @@ def powerset(iterable):
     return chain.from_iterable(combinations(s, r) for r in range(2, len(s)))
 
 
-def getRes(date, campus=['G. Charpak']):
+def readTransportationCosts(path, liste):
+    file = pd.ExcelFile(path)
+    dicTransportationCosts = {}
+    for sheetName in liste:
+        dicTransportationCosts[sheetName] = file.parse(sheetName, index_col=0)
+
+    return dicTransportationCosts
+
+
+def toMatrixTransportationCosts(dicTransportationCosts):
+    transportation_costs = {}
+    for camp in dicTransportationCosts:
+        l = []
+        for col in dicTransportationCosts[camp].columns:
+            row = dicTransportationCosts[camp].loc[col, :].values.tolist()
+            l.append(row)
+        transportation_costs[camp] = l
+    return transportation_costs
+
+
+def writeTransportationCosts(writingPath, dicWeights):
+    with pd.ExcelWriter(writingPath, engine='xlsxwriter', datetime_format='dd mm yyyy') as writer:
+        for camp in dicWeights:
+            dicWeights[camp].to_excel(writer, sheet_name=camp)
+
+
+def readCapacities(path, liste):
+    file = pd.ExcelFile(path)
+    dic = {}
+    for sheetName in liste:
+        dic[sheetName] = file.parse(
+            sheetName, index_col=0).values.tolist()
+        dic[sheetName] = [
+            item for sublist in dic[sheetName] for item in sublist]
+    return dic
+
+
+def toDicCapacities(dicCapacities):
+    res = {}
+    dicProducersLists = getProducersLists()
+    for camp in dicCapacities:
+        buffer = {}
+        i = 0
+        for prod in dicProducersLists[camp]:
+            buffer[prod] = dicCapacities[camp][i]
+            i += 1
+        res[camp] = buffer
+    return res
+
+
+def getProducersLists():
+    CUR_DIR = Path(__file__).parent
+    path = CUR_DIR.parent.parent / 'res' / 'routing' / 'DailyGroupedOrders.xlsx'
+
+    dicFile = readFile(path)
+    dicFile = preProcessing(dicFile)
+    return createDicProducersLists(dicFile)
+
+
+def getDemand(date, campus=['G. Charpak']):
     # File path delaration
     CUR_DIR = Path(__file__).parent
     path = CUR_DIR.parent.parent / 'res' / 'routing' / 'DailyGroupedOrders.xlsx'
@@ -118,4 +176,36 @@ def getRes(date, campus=['G. Charpak']):
     timeInterval = createTimeInterval(dicToProcess, dateMin, dateMax)
 
     res = getCosts(dicToProcess, [dateMin])
-    print(res)
+    return res
+
+
+def getTransportationCosts(liste=['G. Charpak', 'Mines ICM', 'Mines Albi']):
+    # File path delaration
+    CUR_DIR = Path(__file__).parent
+    path = CUR_DIR.parent.parent / 'res' / 'routing' / 'TransportationCosts.xlsx'
+    dicTransportationCosts = readTransportationCosts(path, liste)
+    res = toMatrixTransportationCosts(dicTransportationCosts)
+    return res
+
+
+def getCapacities(liste=['G. Charpak', 'Mines ICM', 'Mines Albi']):
+    CUR_DIR = Path(__file__).parent
+    path = CUR_DIR.parent.parent / 'res' / 'routing' / 'Capacities.xlsx'
+    dicCapacities = readCapacities(path, liste)
+    res = toDicCapacities(dicCapacities)
+    return res
+
+
+def getVehicles(dicCapacities):
+    dicVehicle = {}
+
+    for camp in dicCapacities:
+        buffer = {}
+        for prod in dicCapacities[camp]:
+            if dicCapacities[camp][prod] == 0:
+                buffer[prod] = 0
+            else:
+                buffer[prod] = 1
+        dicVehicle[camp] = buffer
+
+    return dicVehicle
--- lib/routing/main.py
+++ lib/routing/main.py
@@ -3,111 +3,118 @@ import Retailing as rt
 
 date = ['04/01/22']
 campus = ['G. Charpak']
-res = rt.getRes(date)
+res = rt.getDemand(date)
 
-#List of producers
+# List of producers
 Producers = list(res.keys())
 
 supply = res
 
-#if the producers have an available vehicle or not
-vehicle = {Producers[0] : 1,
-          Producers[1] : 0,
-          Producers[2] : 1,
-          Producers[3] : 0,
-          Producers[4] : 0
-        }
+# if the producers have an available vehicle or not
+vehicle = {Producers[0]: 1,
+           Producers[1]: 0,
+           Producers[2]: 1,
+           Producers[3]: 0,
+           Producers[4]: 0
+           }
 
-#capacity of vehicles
-Capacity = {Producers[0] : 5000,
-          Producers[1] : 0,
-          Producers[2] : 3000,
-          Producers[3] : 0,
-          Producers[4] : 0
+# capacity of vehicles
+Capacity = {Producers[0]: 5000,
+            Producers[1]: 0,
+            Producers[2]: 3000,
+            Producers[3]: 0,
+            Producers[4]: 0
             }
 
-#cost matrix. The first index is the campus
-transportation_costs = [[0, 5, 7 , 5, 5, 5],
+# cost matrix. The first index is the campus
+transportation_costs = [[0, 5, 7, 5, 5, 5],
                         [5, 0, 2, 111, 0, 11],
                         [7, 2, 0, 112, 12, 12],
                         [5, 111, 112, 0, 111, 265],
                         [5, 10, 1, 111, 0, 1],
                         [5, 171, 12, 112, 1, 0]
-                       ]
+                        ]
 
-costs = pulp.makeDict([campus + Producers, campus + Producers], transportation_costs, 0)
+costs = pulp.makeDict([campus + Producers, campus +
+                       Producers], transportation_costs, 0)
 
 distance_max = 25
 Np = len(Producers)
 
 Vehicle = [(i, k) for i in campus + Producers for k in Producers]
-Routes = [(i, j, k) for i in campus + Producers for j in campus + Producers for k in Producers]
+Routes = [(i, j, k) for i in campus + Producers for j in campus +
+          Producers for k in Producers]
 
-#lp variable declaration
-vars_routes = pulp.LpVariable.dicts("Route", (campus + Producers, campus + Producers, Producers), 0, None, pulp.LpBinary)
-vars_visit = pulp.LpVariable.dicts("Visit", (campus + Producers, Producers), 0, None, pulp.LpBinary)
+# lp variable declaration
+vars_routes = pulp.LpVariable.dicts(
+    "Route", (campus + Producers, campus + Producers, Producers), 0, None, pulp.LpBinary)
+vars_visit = pulp.LpVariable.dicts(
+    "Visit", (campus + Producers, Producers), 0, None, pulp.LpBinary)
 
-#problem and objective fonction
+# problem and objective fonction
 prob = pulp.LpProblem("Coopain_VRP_Problem", pulp.LpMinimize)
-prob += pulp.lpSum([vars_routes[i][j][k] * costs[i][j] for (i, j, k) in Routes]), "Sum_of_Transporting_Costs"
+prob += pulp.lpSum([vars_routes[i][j][k] * costs[i][j]
+                    for (i, j, k) in Routes]), "Sum_of_Transporting_Costs"
 
-#Producers can only be visited 1 time
+# Producers can only be visited 1 time
 for i in Producers:
-    prob += pulp.lpSum(vars_visit[i][k] for k in Producers) == 1, "visit_of_producers{0}".format(i)
+    prob += pulp.lpSum(vars_visit[i][k]
+                       for k in Producers) == 1, "visit_of_producers{0}".format(i)
 
-#Only 1 route by producer
+# Only 1 route by producer
 for k in Producers:
-    for i in campus + Producers :
+    for i in campus + Producers:
         prob += (pulp.lpSum([vars_routes[i][j][k] for j in campus + Producers])
                  + pulp.lpSum([vars_routes[j][i][k] for j in campus + Producers])) \
-                <= 2, "visit_of_vehicle{0}_{1}".format(k, i)
+            <= 2, "visit_of_vehicle{0}_{1}".format(k, i)
 
-#disable routes from a producer to directely himself
+# disable routes from a producer to directely himself
 for i in campus + Producers:
-    prob += pulp.lpSum([vars_routes[i][i][k] for k in Producers]) == 0, "no_autovisit{0}".format(i)
+    prob += pulp.lpSum([vars_routes[i][i][k]
+                        for k in Producers]) == 0, "no_autovisit{0}".format(i)
 
-#conservation of flux 1
+# conservation of flux 1
 for i in campus + Producers:
     for k in Producers:
         prob += pulp.lpSum([vars_routes[i][j][k] for j in campus + Producers]) \
-                == vars_visit[i][k], "visit_of_producers1_{0}_{1}".format(i,k)
+            == vars_visit[i][k], "visit_of_producers1_{0}_{1}".format(i, k)
 
-#conservation of flux 2
+# conservation of flux 2
 for i in campus + Producers:
     for k in Producers:
         prob += pulp.lpSum([vars_routes[j][i][k] for j in campus + Producers]) \
-                == vars_visit[i][k], "visit_of_producers2_{0}_{1}".format(i,k)
+            == vars_visit[i][k], "visit_of_producers2_{0}_{1}".format(i, k)
 
-#an inexistent vehicle can't do a path
-for k in Producers :
+# an inexistent vehicle can't do a path
+for k in Producers:
     prob += pulp.lpSum([vars_routes[i][j][k] for i in campus + Producers for j in campus + Producers]) \
-                <= 2*Np*vehicle[k], "vehicle_exists{0}".format(k)
+        <= 2*Np*vehicle[k], "vehicle_exists{0}".format(k)
 
-#a producer who uses his vehicle is at the beginnig of a loop
-for k in Producers :
+# a producer who uses his vehicle is at the beginnig of a loop
+for k in Producers:
     prob += pulp.lpSum([vars_visit[i][k] for i in Producers]) \
-                <= 2 * Np * vars_routes[campus[0]][k][k], "first_place{0}".format(k)
+        <= 2 * Np * vars_routes[campus[0]][k][k], "first_place{0}".format(k)
 
-#a producer can't use his vehicle too much
-for k in Producers :
+# a producer can't use his vehicle too much
+for k in Producers:
     prob += pulp.lpSum([vars_routes[i][j][k] * costs[i][j] for i in campus + Producers for j in campus + Producers]) \
-                <= distance_max, "distance_max{0}".format(k)
+        <= distance_max, "distance_max{0}".format(k)
 
-#a vehicule have a capacity
-for k in Producers :
+# a vehicule have a capacity
+for k in Producers:
     prob += pulp.lpSum([vars_visit[i][k] * supply[i] for i in Producers]) \
-                <= Capacity[k], "respect_capacity{0}".format(k)
+        <= Capacity[k], "respect_capacity{0}".format(k)
 
 
-#no underloop
+# no underloop
 for subset in rt.powerset(Producers):
     longueur = len(subset)
     subset_id = ""
     for m in range(longueur):
         subset_id += subset[m]+"_"
-    for k in Producers :
+    for k in Producers:
         prob += pulp.lpSum([vars_routes[i][j][k] for i in subset for j in subset]) \
-                <= longueur-1, "no_underloop_{0}{1}".format(subset_id, k)
+            <= longueur-1, "no_underloop_{0}{1}".format(subset_id, k)
 
 # The problem data is written to an .lp file
 prob.writeLP("CoopainVRPProblem.lp")
