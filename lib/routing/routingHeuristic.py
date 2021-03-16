017bc633a15a136ec1eb0e36710078f59aa84f8b
Yann <poncetyann1@gmail.com>
2021-03-16 16:42:00 +0100

    Add heuristic in a function

--- lib/routing/routingHeuristic.py
+++ lib/routing/routingHeuristic.py
@@ -3,6 +3,7 @@ import itertools
 import numpy as np
 import copy
 
+max_distance = 25
 
 class Prod:
     def __init__(self, name, Id, Group = 0):
@@ -65,7 +66,7 @@ def two_opt(route, cost_mat):
         route = best
     return best
 
-def addNode(road, group):
+def addNode(road, group, GroupRoad, Road, Producers, supply, SatisfiedDemand, Capacity):
     if(len(GroupRoad[group]) == 1): #Add nodes for one truck
             for j in Producers:
                 if((j.getName != road[0].getName)  and Capacity[road[0].getName] >= (SatisfiedDemand[road[0].getName] + supply[j.getName])):
@@ -88,24 +89,113 @@ def addNode(road, group):
                 SatisfiedDemand[cheaper.getName] += supply[i.getName] 
                 visited[i.Id] = 1
 
-    
+
+def processWithHeuristic(readProd, supply, Capacity, transportation_costs):
+    Producers = []
+    for i in readProd:
+        Producers.append(Prod(i, int(i[5:])))
+    Np = len(Producers)
+
+    Road = {}
+    RoadCost = []
+    finalRoad = {}
+    SatisfiedDemand = {}
+    total_demand = 0
+    for i in supply:
+        total_demand += supply[i]
+
+    GroupRoad = {}
+    GroupCapacity = []
+
+
+    #Create road that starts where vehicles are available and group by round
+
+    grp_size=0
+    grp_index = 0
+    for i in Capacity:
+        if(Capacity[i]!=0):
+            
+            if(i not in SatisfiedDemand):
+                SatisfiedDemand[i] = supply[i]
+            else:
+                SatisfiedDemand[i] += supply[i]
+            Road[i] = [Prod(i, int(i[5:]))]
+            if(Capacity[i] >= total_demand):
+                grp_size = 0
+                grp_index += 1
+                GroupRoad[i] = [copy.deepcopy(Road[i][0])]
+                GroupCapacity.append(Capacity[i])
+                
+            elif(Capacity[i] < total_demand or GroupCapacity[i] < total_demand):
+                #SatisfiedDemand[i] +=  supply[i]
+                if(grp_index == 1):
+                    start_prod = i
+                    GroupCapacity.append(Capacity[i])
+
+                    GroupRoad[i] = [copy.deepcopy(Road[i][0])]
+                    grp_index += 1
+                else:
+                    GroupCapacity[grp_index - 1] = Capacity[i] + GroupCapacity[grp_size]
+                    GroupRoad[start_prod].append(Road[i][0])
+
+                    print(grp_index - grp_size)
+                grp_size += 1
+
+    #Add nodes to roads taking into account groups
+    for i in GroupRoad:
+        addNode(Road[i], i, GroupRoad, Road, Producers, supply, SatisfiedDemand, Capacity)
+
+    #Add campus to roads
+    for i in Road:
+        Road[i].append(Prod('Campus', -1))
+
+
+
+    #Costs display
+    for i in Road: 
+        print("Coût de la route", i, ": ", costProcess(Road[i]))
+    for i in SatisfiedDemand:
+        print("Demande de la route", i, ": ", SatisfiedDemand[i])
+
+
+    #Roads enhacement 
+    for i in Road:
+        print(Road[i])
+
+    for i in Road:
+        print("Le camion part de", i)
+        Road[i] = two_opt(Road[i], transportation_costs)
+        print("Route empruntée :", two_opt(Road[i], transportation_costs))
+        print("Le coût est désormais de :", costProcess(Road[i]))
+
+    ind = 0
+    for i in GroupRoad:
+        ind += 1
+        cost = 0
+        for j in GroupRoad[i]:
+            cost += costProcess(Road[j.getName])
+        print("Le coût du groupe ", ind, " est de ", cost)
+
+    return Road, GroupRoad
+
+
+
+
 readProd = ["Prod_0", "Prod_1", "Prod_2", "Prod_3", "Prod_4"]
-Producers = []
-for i in readProd:
-    Producers.append(Prod(i, int(i[5:])))
+
 
 supply = {"Prod_0": 500,
-          "Prod_1": 900,
-          "Prod_2": 1800,
-          "Prod_3": 200,
-          "Prod_4": 700}
+        "Prod_1": 900,
+        "Prod_2": 1800,
+        "Prod_3": 200,
+        "Prod_4": 700}
 
 
 Capacity = {"Prod_0": 5000,
-          "Prod_1": 0,
-          "Prod_2": 3000,
-          "Prod_3": 0,
-          "Prod_4": 1500}
+        "Prod_1": 0,
+        "Prod_2": 3000,
+        "Prod_3": 0,
+        "Prod_4": 1500}
 
 #La dernière colonne/ligne et transportation_costs correspond au campus
 transportation_costs = [[0, 5, 9, 6, 4, 11],
@@ -114,86 +204,11 @@ transportation_costs = [[0, 5, 9, 6, 4, 11],
                         [5, 7, 5, 0, 12, 4],
                         [4, 8, 15, 12, 0, 17],
                         [11, 5, 11, 5, 16, 0]
-                       ]
-
-max_distance = 25
-Np = len(Producers)
-
-Road = {}
-RoadCost = []
-finalRoad = {}
-SatisfiedDemand = {}
-total_demand = 0
-for i in supply:
-    total_demand += supply[i]
-
-GroupRoad = {}
-GroupCapacity = []
-#Create road that starts where vehicles are available and group by round
-
-grp_size=0
-grp_index = 0
-for i in Capacity:
-    if(Capacity[i]!=0):
-        
-        if(i not in SatisfiedDemand):
-            SatisfiedDemand[i] = supply[i]
-        else:
-            SatisfiedDemand[i] += supply[i]
-        Road[i] = [Prod(i, int(i[5:]))]
-        if(Capacity[i] >= total_demand):
-            grp_size = 0
-            grp_index += 1
-            GroupRoad[i] = [copy.deepcopy(Road[i][0])]
-            GroupCapacity.append(Capacity[i])
-            
-        elif(Capacity[i] < total_demand or GroupCapacity[i] < total_demand):
-            #SatisfiedDemand[i] +=  supply[i]
-            if(grp_index == 1):
-                start_prod = i
-                GroupCapacity.append(Capacity[i])
-
-                GroupRoad[i] = [copy.deepcopy(Road[i][0])]
-                grp_index += 1
-            else:
-                GroupCapacity[grp_index - 1] = Capacity[i] + GroupCapacity[grp_size]
-                GroupRoad[start_prod].append(Road[i][0])
-
-                print(grp_index - grp_size)
-            grp_size += 1
-
-#Add nodes to roads taking into account groups
-for i in GroupRoad:
-    addNode(Road[i], i)
-
-#Add campus to roads
-for i in Road:
-    Road[i].append(Prod('Campus', -1))
+                    ]
+                    
 
 
+x, y = processWithHeuristic(readProd, supply, Capacity, transportation_costs)
 
-#Costs display
-for i in Road: 
-    print("Coût de la route", i, ": ", costProcess(Road[i]))
-for i in SatisfiedDemand:
-    print("Demande de la route", i, ": ", SatisfiedDemand[i])
-
-
-#Roads enhacement 
-for i in Road:
-    print(Road[i])
-
-t = two_opt(Road[i], transportation_costs)
-for i in Road:
-    print("Le camion part de", i)
-    Road[i] = two_opt(Road[i], transportation_costs)
-    print("Route empruntée :", two_opt(Road[i], transportation_costs))
-    print("Le coût est désormais de :", costProcess(Road[i]))
-
-ind = 0
-for i in GroupRoad:
-    ind += 1
-    cost = 0
-    for j in GroupRoad[i]:
-        cost += costProcess(Road[j.getName])
-    print("Le coût du groupe ", ind, " est de ", cost)
+print(x)
+print(y)
